9aaa81a5f0346d08a7b802f170fe35a59cb7ab2b
KDzikowicz <karol.dzikowicz@szkola.salezjanie.com>
2021-11-28 12:43:28 +0100

    .

--- insert.php
+++ insert.php
@@ -3,20 +3,20 @@ include "db_connection.php";
  
 if(isset($_POST['submit']))
 {
- $name = $_POST['name'];
- $surname = $_POST['surname'];
- $message = $_POST['message'];
- 
- $insert = mysqli_query($db, "INSERT INTO `kontakt`(`name`, `surname`, `message`) VALUES ('$name', '$surname', '$message')");
- 
- if(!$insert)
- {
- echo mysqli_error();
- }
- else{
- echo "Twoja wiadomość została wysłana";
- }
- 
- mysqli_close($db);
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$message = $_POST['message'];
+
+$insert = mysqli_querry($db, "INSERT INTO `kontakt`(`name`, `surname`, `message`) VALUES ('$name', '$surname', '$message')");
+
+if(!$insert)
+{
+echo mysqli_error();
+}
+else{
+echo "Twoja wiadomość została wysłana";
+}
+
+mysqli_close($db);
 }
 ?>
