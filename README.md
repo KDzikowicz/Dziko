00850a4b87ab01e501f9691ae6601d5ee11edce2
KDzikowicz <karol.dzikowicz@szkola.salezjanie.com>
2021-11-30 10:51:26 +0100

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dziko
+Dziko
---
+++ insert.php
@@ -0,0 +1,22 @@
+<?php
+include "db_connection.php";
+ 
+if(isset($_POST['submit']))
+{
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$message = $_POST['message'];
+
+$insert = mysqli_querry($db, "INSERT INTO `kontakt`(`name`, `surname`, `message`) VALUES ('$name', '$surname', '$message')");
+
+if(!$insert)
+{
+echo mysqli_error();
+}
+else{
+echo "Twoja wiadomość została wysłana";
+}
+
+mysqli_close($db);
+}
+?>
