c27cc097d9281990544edfec36eae6d8b75022c6
KDzikowicz <karol.dzikowicz@szkola.salezjanie.com>
2021-12-14 11:44:42 +0100

    .

--- login.php
+++ login.php
@@ -30,40 +30,41 @@
 <?php
 include "db_connection.php";
 if (isset($_POST['username'], $_POST['password'])){
-$username = $_POST['username'];
-$password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$username = stripcslashes($username);
-$password = stripcslashes($password);
-$username = mysqli_real_escape_string($db, $username);
-$password = mysqli_real_escape_string($db, $password);
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+    $username = mysqli_real_escape_string($db, $username);
+    $password = mysqli_real_escape_string($db, $password);
 
-$sql = "SELECT * FROM accounts where username = '$username' and password = '$password' ";
-$result = mysqli_query($db, $sql);
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$count = mysqli_num_rows($result);
+    $sql = "SELECT * FROM accounts where username = '$username' and password = '$password' ";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
 
-if($count == 1){
-    $_SESSION["username"] = $username;
-    echo "Udane Logowanie" . "<br>" . "<br>";
-}
-if(isset($_SESSION["username"])){
-$sql = "SELECT `id`, `name`, `surname`, `message` FROM kontakt";
-$result = mysqli_query($db, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-// output data of each row
-while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. "<br>" . "Imię: " . $row["name"]. "<br>" . "Nazwisko: " . $row["surname"]. "<br>" . "Tekst: " .  $row["message"] . " " . "<br>" . "<br>";
-}
-} else {
-    echo "Nie ma wyników";
+    if($count == 1){
+        $_SESSION["username"] = $username;
+        echo "Udane Logowanie" . "<br>" . "<br>";
     }
-}
-else{
-echo "Błędny login lub hasło";
+    if(isset($_SESSION["username"])){
+        $sql = "SELECT `id`, `name`, `surname`, `message` FROM kontakt";
+        $result = mysqli_query($db, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "id: " . $row["id"]. "<br>" . "Imię: " . $row["name"]. "<br>" . "Nazwisko: " . $row["surname"]. "<br>" . "Tekst: " .  $row["message"] . " " . "<br>" . "<br>";
+            }
+        }
+        else{
+        echo "Nie ma wyników";
+        }
     }
-}
+    else{
+    echo "Błędny login lub hasło";
+        }
+    }
 else{
     echo "Sesja wygasła";
     }
